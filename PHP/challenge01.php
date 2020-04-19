eed396b18cad902570ed0b8a161c6f7805835676
nely de faria <nelly.nf54@gmail.com>
2020-04-19 12:35:11 -0300

    01 - Add first challenge

---
+++ PHP/challenge01.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    //Informe seu código aqui
+    $texto = "Treinaweb";
+    $numero = 7;
+    $pontoFlutuante = 420.08;
+
+
+    echo $texto . PHP_EOL;
+    echo $numero . PHP_EOL;
+    echo $pontoFlutuante . PHP_EOL;
+    echo gettype($texto) . PHP_EOL;
+    echo gettype($numero) . PHP_EOL;
+    echo gettype($pontoFlutuante);
