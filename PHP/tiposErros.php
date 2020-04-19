11026ee8b6380275a32b1b39503f11fb45c4743f
nely de faria <nelly.nf54@gmail.com>
2020-04-19 12:59:27 -0300

    09 - Add file tiposErros.php

---
+++ PHP/tiposErros.php
@@ -0,0 +1,10 @@
+<?php
+
+error_reporting(E_ALL);
+
+/*
+* Notice – Aviso: É o erro mais simples que o PHP gera, apenas está avisando que algo não está certo, porém não chega a atrapalhar diretamente a execução do programa;
+* Warning – Alerta: É algo que precisamos ficar atentos, pois apesar de não parar a execução do programa, pode gerar graves problemas nas nossas aplicações;
+* Fatal - Erro fatal: Esse erro impede completamente o programa de continuar sua execução, por esse motivo o script é interrompido;
+* Parser - Erro de sintaxe: Esse erro acontece quando escrevemos um código inválido para o interpretador PHP. O programa é interrompido, pois o interpretador não sabe o que deve ser feito, já que não entende o código.
+*/
